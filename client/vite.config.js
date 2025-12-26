ab5e576ac8e9ea4cbf75cba2bd26ace574d5b43d
AniketSupekar <aniketsupekar2003@gmail.com>
2025-12-26 15:53:18 +0530

    Added target URL

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "https://balaji-nursery.onrender.com/",
         changeOrigin: true
       }
     }
