02ba2af3902df9e55976b50724a852e7396b7b23
AniketSupekar <aniketsupekar2003@gmail.com>
2026-01-08 15:30:31 +0530

    Corrected the base URL in api

--- client/src/services/auth.api.js
+++ client/src/services/auth.api.js
@@ -1,8 +1,13 @@
 import axios from "axios";
 
+const BASE_URL =
+  import.meta.env.MODE === "development"
+    ? "/api"
+    : "https://nursery-app-iin1.onrender.com/api";
+
 const api = axios.create({
-  baseURL: "/api",
-  withCredentials: true
+  baseURL: BASE_URL,
+  withCredentials: true,
 });
 
 export const login = (data) => api.post("/auth/login", data);
