15b5df884b5c6f95058dc244ce535d3ff4ee3640
AniketSupekar <aniketsupekar2003@gmail.com>
2026-01-30 14:14:44 +0530

    Change in redis.js

--- server/src/utils/redis.js
+++ server/src/utils/redis.js
@@ -1,18 +1,30 @@
 import Redis from "ioredis";
 
+let redis = null;
+
 const redisUrl =
   process.env.NODE_ENV === "production"
     ? process.env.REDIS_PROD_URL
     : process.env.REDIS_LOCAL_URL;
 
-const redis = new Redis(redisUrl);
+if (redisUrl) {
+  redis = new Redis(redisUrl);
 
-redis.on("connect", () => {
-  console.log(`✅ Redis connected (${process.env.NODE_ENV})`);
-});
+  redis.on("connect", () => {
+    console.log(`✅ Redis connected (${process.env.NODE_ENV})`);
+  });
 
-redis.on("error", (err) => {
-  console.error("❌ Redis error:", err);
-});
+  redis.on("error", (err) => {
+    console.error("❌ Redis error:", err.message);
+  });
+} else {
+  console.warn(
+    `⚠️ Redis disabled (no ${
+      process.env.NODE_ENV === "production"
+        ? "REDIS_PROD_URL"
+        : "REDIS_LOCAL_URL"
+    } found)`
+  );
+}
 
 export default redis;
